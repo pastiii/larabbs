8258f8d970184515d890c81661f35e1170078693
zhuzhu <593343693@qq.com>
2018-05-14 09:25:38 +0800

    edit welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    he is a baby!!
                 </div>
 
                 <div class="links">
