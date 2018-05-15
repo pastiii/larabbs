ea62693ef4da994fae4a7f7dbe49bfce5f3c5e72
zhuzhu <593343693@qq.com>
2018-05-15 10:02:24 +0800

    add userss

---
+++ app/Http/Controllers/UserssController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserssController extends Controller
+{
+    public function index(){
+    	return "我是朱小强";
+    }
+}
